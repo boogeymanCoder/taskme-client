99b5b9081956369c41859c94c206c51b6b266567
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-11-21 20:05:57 +0800

    asynchronized api functions

--- src/api/task.js
+++ src/api/task.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-export function createTask(body) {
+export async function createTask(body) {
   return axios.post(`${process.env.REACT_APP_API_HOST}/api/task`, body, {
     withCredentials: true,
   });
+}
+
+// TODO find n documents at a time
+export async function findTaskBatch(batchLimit, batchNum) {
+  return axios.get(
+    `${process.env.REACT_APP_API_HOST}/api/task/batch/${batchLimit}/${batchNum}`,
+    { withCredentials: true }
+  );
 }
