b0c34ac3cf8a73a37e55c2f0a9f9d7df5665d9f2
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-12-05 11:24:28 +0800

    added cconversation next and prev

--- src/Components/ConversationList.js
+++ src/Components/ConversationList.js
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Pagination from "./Pagination";
 
 export default function ConversationList() {
   const inbox = useSelector((state) => state.inbox.inbox);
+  const [conversations, setConversations] = useState();
+  const [conversationBatch, setConversationBatch] = useState(1);
+  const [enableNext, setEnableNext] = useState(false);
+  const [enablePrevious, setEnablePrevious] = useState(false);
+
+  useEffect(() => {
+    if (!inbox) return null;
+    setConversations((lastState) => {
+      const updatedConversations = inbox.slice(
+        (conversationBatch - 1) * 20,
+        conversationBatch * 20
+      );
+
+      if (updatedConversations.length === 20) setEnableNext(true);
+      else setEnableNext(false);
+
+      if (conversationBatch <= 1) setEnablePrevious(false);
+      else setEnablePrevious(true);
+
+      return updatedConversations;
+    });
+  }, [inbox, conversationBatch]);
 
   function renderConversations() {
-    if (inbox) {
-      return inbox.map((conversation) => (
-        <div key={conversation._id}>
-          <Link to={`conversation/${conversation._id}`}>
-            {conversation.name}
-          </Link>
-        </div>
-      ));
-    } else {
-      return <h2>No Messages Yet</h2>;
-    }
+    if (!conversations) return <h2>Loading...</h2>;
+    else if (conversations.length < 1) return <h2>No Conversations Yet</h2>;
+    return conversations.map((conversation) => (
+      <div key={conversation._id}>
+        <Link to={`conversation/${conversation._id}`}>{conversation.name}</Link>
+      </div>
+    ));
   }
 
-  return <div>{renderConversations()}</div>;
+  return (
+    <div>
+      {renderConversations()}
+
+      <Pagination
+        setPage={setConversations}
+        setPageBatch={setConversationBatch}
+        enablePrevious={enablePrevious}
+        setEnablePrevious={setEnablePrevious}
+        enableNext={enableNext}
+        setEnableNext={setEnableNext}
+      />
+    </div>
+  );
 }
